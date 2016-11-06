e89dad431bd795e266cb7f17162ea195007810fd
Ken Duigraha Putra <email": null>
2016-11-06 20:14:35 +0700

    installed chai-http

--- server/test/test.index.js
+++ server/test/test.index.js
@@ -3,6 +3,9 @@ const chai = require('chai')
 const assert = chai.assert
 const expect = chai.expect
 const controller = require('../controllers/controller.api.memo')
+const chaiHTTP = require('chai-http')
+chai.use(chaiHTTP)
+const should = chai.should()
 
 describe('#multiple', () => {
   it('it should return the multiple value of a & b params', () => {
